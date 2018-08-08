ff4e5b9aaccf5ce45046c4f777de8f6967be92e9
Richard Dubay <richardtdubayjr@gmail.com>
2018-08-08 13:44:58 -0400

    Revert "fix caching and breaking attendance occurrence"

--- Utility/Helpers.cs
+++ Utility/Helpers.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.UI.WebControls;
-using Rock.Cache;
+using Rock.Web.Cache;
 using Rock.Web.UI.Controls;
 using Rock;
 using Rock.CheckIn;
@@ -17,7 +17,7 @@ namespace cc.newspring.AttendedCheckIn.Utility
         /// <returns></returns>
         public static List<ListItem> GetAbilityItems()
         {
-            var abilities = CacheDefinedType.Get( new Guid( Rock.SystemGuid.DefinedType.PERSON_ABILITY_LEVEL_TYPE ) ).DefinedValues;
+            var abilities = DefinedTypeCache.Read( new Guid( Rock.SystemGuid.DefinedType.PERSON_ABILITY_LEVEL_TYPE ) ).DefinedValues;
 
             if ( abilities.Count > 0 )
             {
@@ -33,7 +33,7 @@ namespace cc.newspring.AttendedCheckIn.Utility
         /// <returns></returns>
         public static List<ListItem> GetGradeItems()
         {
-            var grades = CacheDefinedType.Get( new Guid( Rock.SystemGuid.DefinedType.SCHOOL_GRADES ) ).DefinedValues;
+            var grades = DefinedTypeCache.Read( new Guid( Rock.SystemGuid.DefinedType.SCHOOL_GRADES ) ).DefinedValues;
             if ( grades.Count > 0 )
             {
                 return grades.Select( dv => new ListItem( dv.Description, dv.Value.ToString() ) ).ToList();
@@ -104,7 +104,7 @@ namespace cc.newspring.AttendedCheckIn.Utility
         public static int ReadAttendanceBySchedule( int locationId, int? scheduleId )
         {
             var attendanceCount = 0;
-            var attendanceCache = KioskLocationAttendance.Get( locationId );
+            var attendanceCache = KioskLocationAttendance.Read( locationId );
 
             if ( attendanceCache != null )
             {
